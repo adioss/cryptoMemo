b6bf84115a88d2a53444f3ce782bebb3c67ef354
adioss <adrien.pailhes@adioss.fr>
2015-09-25 13:09:31 +0200

    Append simple test complete symmetric test with AES and a file to encrypt

---
+++ src/main/java/com/adioss/security/symmetric/SimpleSymmetricEncryptDecrypt.java
@@ -0,0 +1,46 @@
+package com.adioss.security.symmetric;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+public class SimpleSymmetricEncryptDecrypt {
+
+    public void encrypt(File inputFile, File outputFile, byte[] key, String algorithm, String transformation) {
+        doCrypto(inputFile, outputFile, Cipher.ENCRYPT_MODE, key, algorithm, transformation);
+    }
+
+    public void decrypt(File inputFile, File outputFile, byte[] key, String algorithm, String transformation) {
+        doCrypto(inputFile, outputFile, Cipher.DECRYPT_MODE, key, algorithm, transformation);
+    }
+
+    private static void doCrypto(File inputFile, File outputFile, int cipherMode, byte[] key, String algorithm, String transformation) {
+        try {
+            Key secretKey = new SecretKeySpec(key, algorithm);
+            Cipher cipher = Cipher.getInstance(transformation);
+            cipher.init(cipherMode, secretKey);
+
+            try (FileInputStream inputStream = new FileInputStream(inputFile); FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+                byte[] inputBytes = new byte[(int) inputFile.length()];
+                inputStream.read(inputBytes);
+                byte[] outputBytes = cipher.doFinal(inputBytes);
+                outputStream.write(outputBytes);
+            }
+
+
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | IOException ex) {
+            throw new RuntimeException("Error encrypting/decrypting file", ex);
+        }
+    }
+}
---
+++ src/test/java/com/adioss/security/symmetric/SimpleSymmetricEncryptDecryptTest.java
@@ -0,0 +1,63 @@
+package com.adioss.security.symmetric;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SimpleSymmetricEncryptDecryptTest {
+    private static final String CLEAR_FILE_NAME = "/clearFile.txt";
+    private static final String ENCRYPTED_FILE_NAME = "/encryptedFile.txt";
+    private static final String TEMP_FILE_NAME = "/outputFile.txt";
+    // key size must be 16 or 128 for AES
+    private static final String KEY = "Mary has one cat";
+    public static final String KEY_ALGORITHM = "AES";
+    public static final String CIPHER_TRANSFORMATION_ALGORITHM = "AES";
+
+    @Rule
+    public TemporaryFolder testFolder = new TemporaryFolder();
+
+    @Test
+    public void testEncrypt() throws Exception {
+        // Given
+        File inputFile = loadFile(CLEAR_FILE_NAME);
+        File outputFile = testFolder.newFile(TEMP_FILE_NAME);
+        SimpleSymmetricEncryptDecrypt encryptDecrypt = new SimpleSymmetricEncryptDecrypt();
+
+        // When
+        encryptDecrypt.encrypt(inputFile, outputFile, KEY.getBytes(), KEY_ALGORITHM, CIPHER_TRANSFORMATION_ALGORITHM);
+
+        // Then
+        Assert.assertEquals(fileToString(outputFile), fileToString(loadFile(ENCRYPTED_FILE_NAME)));
+    }
+
+    @Test
+    public void testDecrypt() throws Exception {
+        // Given
+        File inputFile = loadFile(ENCRYPTED_FILE_NAME);
+        File outputFile = testFolder.newFile(TEMP_FILE_NAME);
+        SimpleSymmetricEncryptDecrypt encryptDecrypt = new SimpleSymmetricEncryptDecrypt();
+
+        // When
+        encryptDecrypt.decrypt(inputFile, outputFile, KEY.getBytes(), KEY_ALGORITHM, CIPHER_TRANSFORMATION_ALGORITHM);
+
+        // Then
+        Assert.assertEquals(fileToString(outputFile), fileToString(loadFile(CLEAR_FILE_NAME)));
+    }
+
+    private String fileToString(File outputFile) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(outputFile.getPath())));
+    }
+
+    private File loadFile(String fileName) throws IOException, URISyntaxException {
+        URL resourceUrl = getClass().getResource(fileName);
+        return new File(resourceUrl.toURI());
+    }
+}
